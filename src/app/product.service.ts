192cfbfa043e7f8a220f550c0d419840a410918e
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-07 18:14:29 +0100

    Completed module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import  'rxjs/add/operator/map';
 import { Album } from './album';
 import {Observable} from 'rxjs/Observable';
+import { Product } from './product';
 
 
 
@@ -12,15 +13,24 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
     getAlbum  (id:number) : Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>
        response.json());
 
+    }
+    getProducts(){
+
+      return this._http.get(this._productsUrl).map((response=><Product[]>response.json()))
+    };
+
+      
+    
        
     
 
     
-  }
+  
 
 }
